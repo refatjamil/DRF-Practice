6cf14dbe9b0788cd40430876d757b60f83a3d7ad
Rifat Jamil <rifatjamil54@gmail.com>
2023-03-27 09:29:44 +0600

    cmt

---
+++ GenericAPIViewandMixins/api/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+
+
+class StudentList(GenericAPIView, ListModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+class StudentCreateModelMixin(GenericAPIView, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)    
+    
+class StudentRetrieveModelMixin(GenericAPIView, RetrieveModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)   
+    
+class StudentUpdateModelMixin(GenericAPIView, UpdateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)    
+    
+class StudentDestroyModelMixin(GenericAPIView, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)    
